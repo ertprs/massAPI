b88fade40d1b1b6e760301a68d547adde9553a9d
serGem811 <ser.gem.811@outlook.com>
2020-01-11 08:49:36 -0500

    test

--- api/responsewapp/controllers/Responsewapp.js
+++ api/responsewapp/controllers/Responsewapp.js
@@ -24,7 +24,7 @@ module.exports = {
       let event = ctx.request.body.messages[0];
       if (event.type === "chat" && event.body && !event.fromMe) {
         console.log('hook chat api');
-        console.log(ctx.request);
+        console.log(ctx.request.body);
         console.log('--------------------------');
         const finded = await findMessage(event.body);
         console.log(finded);
